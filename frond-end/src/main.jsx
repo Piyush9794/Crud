df354f77aa3d96870c9f6a4759c0e8fc4e0050fd
Piyush <vishwakarmapiyush327@gmail.com>
2026-08-12 20:58:09 +0530

    change vit2e

--- frond-end/src/main.jsx
+++ frond-end/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/Crud">
+  <BrowserRouter >
     <App />
     <ToastContainer
       position="top-right"
